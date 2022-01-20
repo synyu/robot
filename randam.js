702c72d5c9a4d19e52198eb1a6c00806006e204c
synyu <98072781+synyu@users.noreply.github.com>
2022-01-20 17:16:07 +0900

    Update randam.js

--- randam.js
+++ randam.js
@@ -1,5 +1,5 @@
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
-var links = ['https://forms.gle/oe4pyo7BQRSeuSYp8','https://forms.gle/Y7Mn4a9fQvzSeLU19']; //同じようにして遷移先のURLを配列へ格納する
+var links = ['https://forms.gle/oe4pyo7BQRSeuSYp8','https://forms.gle/dEeZonUBVN2Yr3iD8','https://forms.gle/5Co5kpaLPEDGZxMy5','https://forms.gle/BnLZzBxQLDp5ufQg7','https://forms.gle/kRixiQ8xMV2pY73w6','https://forms.gle/SBaMAtJD2ULTfkZu5','https://forms.gle/JBDGcxv6VUNoXkyc8','https://forms.gle/xUgZ4scvzh7hCtSY8','https://forms.gle/ApmKQX4VpZ67CU7Y6','https://forms.gle/2w8vUVFPsNJfXPSv9','https://forms.gle/hLp4XmmZZssd8C8FA','https://forms.gle/kur9tVFSEDfvLehA8','https://forms.gle/cXDE5jfhC3qa1wad6','https://forms.gle/Hw5dBpKQNZN17oyX8','https://forms.gle/9GkwWQxCZbmBcBKr6','https://forms.gle/9WtVqCCAz6aeHc3M8','https://forms.gle/tNQ2PD4GJNvGwWCe6','https://forms.gle/CFBkutPjocsFb5CJ6','https://forms.gle/w51yR2gXL8ZpAwEw6','https://forms.gle/gwYhsVaRJP1tu3jm6','https://forms.gle/4gT2CPyCxNggn9nP8','https://forms.gle/E5LP9BWVnHKYzcXv5','https://forms.gle/KZk8ophWWQUFitBM9','https://forms.gle/DeP5YbkirksWrHYK8','https://forms.gle/Ht2M5RU7p5vEMh1D9'];
 window.location.href = links[getRandomInt(links.length)]; 
