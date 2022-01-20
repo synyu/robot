8b3600fa176ed4b5fd6e3e9b9fa95dbe8efc4608
synyu <98072781+synyu@users.noreply.github.com>
2022-01-20 14:54:48 +0900

    Update randam.js

--- randam.js
+++ randam.js
@@ -1,5 +1,5 @@
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
-var links = ['https://forms.gle/oe4pyo7BQRSeuSYp8']; //同じようにして遷移先のURLを配列へ格納する
+var links = ['https://forms.gle/oe4pyo7BQRSeuSYp8','https://forms.gle/Y7Mn4a9fQvzSeLU19']; //同じようにして遷移先のURLを配列へ格納する
 window.location.href = links[getRandomInt(links.length)]; 
